4e9b5f64e702a78981d250f665e3d13392673d71
Cassio <cassiodourado1@gmail.com>
2014-03-26 18:54:28 -0300

    testando...
    testando testando...

---
+++ exercicio2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+	int numInformado;
+	printf("Informe um numero: ");
+	scanf("%d", &numInformado);
+	printf("\n");
+	printf("O numero informado e %d.", numInformado);
+	getchar();
+	return(0);
+
+}
